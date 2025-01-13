fc751cc625318663a6ccfbb67f132a53ea97edbc
JPrime1 <james.prime.dev@gmail.com>
2025-01-13 06:04:47 -0500

    added documentation to seedGen class

--- LibraryOfBabel/src/core/SeedGenerator.cs
+++ LibraryOfBabel/src/core/SeedGenerator.cs
@@ -16,11 +16,21 @@ namespace LibraryOfBabel.src.core
             return 0;
         }
 
+        /// <summary>
+        /// Generates psudorandom seed value dependent on BookID attributes
+        /// </summary>
+        /// <param name="book">Book Parameter</param>
+        /// <returns>Seed Integer</returns>
         public static int GenerateSeed(BookId book)
         {
             return SeedGenerator.CalculateSeed(book);
         }
 
+        /// <summary>
+        /// Internal helper function, calculating the seed value given BookID
+        /// </summary>
+        /// <param name="book">Book Parameter</param>
+        /// <returns>Calculated Int Seed Result</returns>
         private static int CalculateSeed(BookId book)
         {
             return book.RoomX * 73856093 ^ book.RoomY * 19349663 ^ book.WallDirection * 83492791 ^ book.ShelfLevel * 1234567 ^ book.BookNumber * 9876543 ^ book.CurrentPage * 54321;
